0dcdc2dd1e0b0fd53b3fc12b7c408042501711ea
abteilung6 <moeller.tilman@gmail.com>
2022-03-28 17:06:58 +0200

    feat: delete and retrieve friendship

--- tilmanx/chat/views/friendship.py
+++ tilmanx/chat/views/friendship.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from rest_framework import mixins, permissions
 from rest_framework.decorators import action
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -42,6 +43,16 @@ class FriendshipViewSet(
         serializer = self.get_serializer(friendships, many=True)
         return Response(serializer.data)
 
+    def retrieve(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        queryset: QuerySet = self.filter_queryset(self.get_queryset())
+        friendship = get_object_or_404(
+            queryset,
+            Q(pk=pk) & (Q(requester=self.request.user) | Q(addressee=self.request.user))
+        )
+        serializer = self.get_serializer(friendship)
+        return Response(serializer.data)
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -50,8 +61,16 @@ class FriendshipViewSet(
 
     @action(detail=True, methods=['PUT'])
     def accept(self, request, pk=None):
-        friendship = Friendship.objects.get(id=pk)
+        friendship = self.get_object()
         serializer = self.get_serializer(friendship, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, *args, **kwargs):
+        instance: Friendship = self.get_object()
+        if instance.requester == self.request.user or instance.addressee == self.request.user:
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
--- tilmanx/chat/views/test_friendship.py
+++ tilmanx/chat/views/test_friendship.py
@@ -14,8 +14,10 @@ class TestFriendshipViewSet(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         self.view_list = FriendshipViewSet.as_view(actions={'get': 'list'})
+        self.view_retrieve = FriendshipViewSet.as_view(actions={'get': 'retrieve'})
         self.view_create = FriendshipViewSet.as_view(actions={'post': 'create'})
         self.view_accept = FriendshipViewSet.as_view(actions={'put': 'accept'})
+        self.view_delete = FriendshipViewSet.as_view(actions={'delete': 'destroy'})
         self.requester = User.objects.create(username='requester')
         self.another_requester = User.objects.create(username='another_requester')
         self.addressee = User.objects.create(username='addressee')
@@ -32,8 +34,20 @@ class TestFriendshipViewSet(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
-    def test_create(self):
+    def test_retrieve(self):
+        # Arrange
+        friendship = Friendship.objects.create(requester=self.requester, addressee=self.addressee)
+        request = self.factory.get(f'/api/friendships/{friendship.id}/accept/')
+        force_authenticate(request, user=self.requester)
         # Act
+        response: Response = self.view_retrieve(request, pk=friendship.id)
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['requester_id'], self.requester.id)
+        self.assertEqual(response.data['addressee_id'], self.addressee.id)
+
+    def test_create(self):
+        # Arrange
         data = {
             'addressee_id': self.addressee.id
         }
@@ -52,9 +66,19 @@ class TestFriendshipViewSet(TestCase):
         request = self.factory.put(f'/api/friendships/{friendship.id}/accept/')
         force_authenticate(request, user=self.addressee)
         # Act
-        response: Response = self.view_accept(request, friendship.id)
+        response: Response = self.view_accept(request, pk=friendship.id)
         # Arrange
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['requester_id'], self.requester.id)
         self.assertEqual(response.data['addressee_id'], self.addressee.id)
         self.assertEqual(response.data['status'], FriendshipStatus.ACCEPTED.value)
+
+    def test_delete(self):
+        # Arrange
+        friendship = Friendship.objects.create(requester=self.requester, addressee=self.addressee)
+        request = self.factory.delete(f'/api/friendships/{friendship.id}/')
+        force_authenticate(request, user=self.addressee)
+        # Act
+        response: Response = self.view_delete(request, pk=friendship.id)
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
