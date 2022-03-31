019978001f6bb313bc91aa85b07c1d206f754243
abteilung6 <moeller.tilman@gmail.com>
2022-03-31 15:43:30 +0200

    feat: logout icon in settings

--- TilmanxApp/src/screens/SettingsScreen.tsx
+++ TilmanxApp/src/screens/SettingsScreen.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 
 import {RootStackParamList} from '../navigation/types';
+import {useAuthentication} from '../hooks/authentication/useAuthentication';
+import {defaultTheme} from '../styles/theme';
+import Logo from '../assets/icon-logout.svg';
 
 export const SettingsScreen: React.FC<
   StackScreenProps<RootStackParamList, 'Settings'>
 > = () => {
+  const {logout, loading} = useAuthentication();
   const render = (): React.ReactElement => {
     return (
       <View style={styles.container}>
-        <Text>Settings</Text>
+        <Text style={[defaultTheme.typography.h3, styles.title]}>Settings</Text>
+        <Pressable style={styles.pressable} disabled={loading} onPress={logout}>
+          <Logo width={24} height={24} fill={'red'} color="red" />
+          <View style={styles.textView}>
+            <Text style={[defaultTheme.typography.body1]}>Sign Out</Text>
+          </View>
+        </Pressable>
       </View>
     );
   };
@@ -21,5 +31,17 @@ export const SettingsScreen: React.FC<
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 15,
+    marginHorizontal: 10,
+  },
+  title: {
+    marginBottom: 10,
+  },
+  pressable: {
+    flexDirection: 'row',
+  },
+  textView: {
+    justifyContent: 'center',
+    marginHorizontal: 10,
   },
 });
--- TilmanxApp/src/styles/typography.ts
+++ TilmanxApp/src/styles/typography.ts
@@ -2,7 +2,7 @@ import {TextStyle} from 'react-native';
 
 import {SolidColor} from './color';
 
-export type TypographyVariant = 'h1' | 'h2' | 'body1';
+export type TypographyVariant = 'h1' | 'h2' | 'h3' | 'body1';
 
 export const typography: Record<TypographyVariant, TextStyle> = {
   h1: {
@@ -19,6 +19,14 @@ export const typography: Record<TypographyVariant, TextStyle> = {
     fontSize: 32,
     lineHeight: 48,
     letterSpacing: -1,
+    color: SolidColor.gray,
+  },
+  h3: {
+    fontStyle: 'normal',
+    fontWeight: '700',
+    fontSize: 24,
+    lineHeight: 40,
+    letterSpacing: -0.75,
     color: SolidColor.gray,
   },
   body1: {
