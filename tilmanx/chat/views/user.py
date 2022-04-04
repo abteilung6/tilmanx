585d84c52002b936ad9008426777d09cd6f39548
abteilung6 <moeller.tilman@gmail.com>
2022-04-04 20:31:38 +0200

    wip: search for users
    * by username, first_name, last_name

--- tilmanx/chat/views/user.py
+++ tilmanx/chat/views/user.py
@@ -1,18 +1,21 @@
 from django.contrib.auth.models import User
 
-from rest_framework import permissions
-from rest_framework.decorators import action
+from rest_framework import permissions, mixins
+from rest_framework import filters
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from chat.serializers import UserSerializer
 
 
-class UserViewSet(GenericViewSet):
+class UserViewSet(mixins.ListModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'first_name', 'last_name']
 
     @action(detail=False, methods=['GET'])
     def me(self, request, pk=None):
