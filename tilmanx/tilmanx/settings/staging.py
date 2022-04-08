8b3eab4b52b1518d97b40cc229c1c6a32aa43d6f
abteilung6 <moeller.tilman@gmail.com>
2022-04-08 14:34:18 +0200

    fix: ignore collectstatic argument

--- tilmanx/tilmanx/settings/staging.py
+++ tilmanx/tilmanx/settings/staging.py
@@ -1,7 +1,11 @@
 import os
 import dj_database_url
+from pathlib import Path
+import sys
 
 from django.core.management.utils import get_random_secret_key
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 """
 If you don’t set this environment variable, then every time the app is re-deployed, this key will change. 
@@ -17,9 +21,21 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # database
-if os.getenv("DATABASE_URL", None) is None:
-    raise Exception("DATABASE_URL environment variable not defined")
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-}
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
