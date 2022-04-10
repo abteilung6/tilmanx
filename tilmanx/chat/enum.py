d3d4ac631802f66559de93b2567930b7ec86d405
abteilung6 <moeller.tilman@gmail.com>
2022-04-10 11:34:25 +0200

    feat: add `type` field to `Conversation`

--- tilmanx/chat/enum.py
+++ tilmanx/chat/enum.py
@@ -2,6 +2,13 @@ from enum import Enum, unique
 
 
 @unique
+class ModelEnum(Enum):
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+
+@unique
 class FriendshipStatus(Enum):
     OFFERED = 'OFFERED'
     """Requester has offered the friendship."""
@@ -13,3 +20,11 @@ class FriendshipStatus(Enum):
 class FriendshipAction(Enum):
     ACCEPT = 'ACCEPT'
     """Addressee accepts the friendship."""
+
+
+@unique
+class ConversationType(ModelEnum):
+    PRIVATE = 'PRIVATE'
+    """A private conversation consists of two participants"""
+    GROUP = 'GROUP'
+    """A group conversation consists of finite participants"""
--- tilmanx/chat/models/conversation.py
+++ tilmanx/chat/models/conversation.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from typing import TYPE_CHECKING
+
+from chat.enum import ConversationType
+
+if TYPE_CHECKING:
+    from chat.models import Friendship
 
 
 class Conversation(models.Model):
+    type = models.CharField(max_length=48, choices=ConversationType.choices(), default=ConversationType.PRIVATE.value)
+    """The type determines weather the conversation is a private or group conversation"""
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
